0cf124283cf6d7df00a77dcf1669669e994a8a08
Suchitra Sahoo <suchitrasahoo874@gmail.com>
2025-09-09 19:17:39 +0530

    added route for signup and signin in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 
 const App = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-gray-500 text-5xl text-center">
-        We are creating a smart lost and found system
-      </h1>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="h-screen flex items-center justify-center">
+            <h1 className="text-gray-500 text-5xl text-center">
+              We are creating a smart lost and found system
+            </h1>
+          </div>
+        }
+      />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+    </Routes>
   );
-}
+};
 
 export default App;
