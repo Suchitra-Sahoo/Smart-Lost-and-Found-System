d351bfd93ef1cd0e924536cce61604ab5943230c
Suchitra Sahoo <suchitrasahoo874@gmail.com>
2025-09-13 12:29:11 +0530

    Added folder and route of landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,20 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
+import LandingPage from "./pages/LandingPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <div className="h-screen flex items-center justify-center">
-            <h1 className="text-gray-500 text-5xl text-center">
-              We are creating a smart lost and found system
-            </h1>
-          </div>
-        }
-      />
+      <Route path="/" element={<LandingPage/>} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
     </Routes>
