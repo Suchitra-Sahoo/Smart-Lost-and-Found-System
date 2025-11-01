498f421dd6e71cd3a051a689b040616fde027450
Suchitra Sahoo <suchitrasahoo874@gmail.com>
2025-11-01 13:24:23 +0530

    added padding in admin dashbaord

--- Frontend/src/components/admin-dashboard/TopSection.jsx
+++ Frontend/src/components/admin-dashboard/TopSection.jsx
@@ -33,7 +33,7 @@ const TopSection = () => {
   }, []);
 
   return (
-    <div>
+    <div className="pt-8 px-6">
       <h1 className="text-3xl font-bold mb-5 text-orange-600">Admin Dashboard</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
